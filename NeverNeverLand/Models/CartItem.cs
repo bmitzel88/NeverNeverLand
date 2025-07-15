ac1d77a9c3f95f09c5a2ab92ae8cad9d8fe82356
Brandon <brandonmitzel05@gmail.com>
2025-07-15 12:36:53 -0700

    Created a model for each item within the cart

---
+++ NeverNeverLand/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace NeverNeverLand.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; } // Unique ID for the cart item
+        public required string ItemType { get; set; } // "Ticket", "Membership", "Product"
+        public required string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
