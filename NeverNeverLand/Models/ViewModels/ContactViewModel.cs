bcee3a12280c091557203a92bce04b25d32fa374
Brandon <brandonmitzel05@gmail.com>
2025-08-30 17:10:18 -0700

    Contact, About, and Events pages now function

---
+++ NeverNeverLand/Models/ViewModels/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeverNeverLand.Models.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required, StringLength(80)]
+        public string Name { get; set; } = "";
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required, StringLength(120)]
+        public string Subject { get; set; } = "";
+
+        [Required, StringLength(4000)]
+        public string Message { get; set; } = "";
+
+        // Honeypot (not bound to view directly)
+        // public string? Website { get; set; }
+
+        [Required(ErrorMessage = "Please agree to the Privacy Policy.")]
+        public bool AgreeToPrivacy { get; set; }
+    }
+}
---
+++ NeverNeverLand/Models/ViewModels/EventsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NeverNeverLand.Models.ViewModels
+{
+    public record EventsViewModel
+    {
+        public string? ImageUrl { get; init; }
+        public string Title { get; init; } = "";
+        public DateTimeOffset StartLocal { get; init; }
+        public DateTimeOffset? EndLocal { get; init; }
+        public string? Location { get; init; }
+        public string? Description { get; init; }
+    }
+}
